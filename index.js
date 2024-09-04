4823862a09e8e105570157e4b0096c05f6002da2
Monica Alyssa <monica_alyssa@icloud.com>
2024-09-04 18:33:27 -0400

    Add CORS' allowed origins

--- index.js
+++ index.js
@@ -47,6 +47,19 @@ app.use(morgan("combined", { stream: accessLogStream }));
 // takes specific pathname "/" requests from the user and uses express to route them to any file located in the public folder
 app.use(express.static("public"));
 
+let allowedOrigins = ['http://localhost:1234', 'https://popcornpal.netlify.app/'];
+
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn’t found on the list of allowed origins
+      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+      return callback(new Error(message), false);
+    }
+    return callback(null, true);
+  }
+}));
+
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "/public/documentation.html"));
 });
